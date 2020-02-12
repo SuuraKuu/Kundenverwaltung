404e404245b27e8a0660595e31704f7a999aac18
Keimeno <constantin@metzworld.com>
2020-02-12 16:45:40 +0100

    updated tests

--- src/test/java/KundenverwaltungTest.java
+++ src/test/java/KundenverwaltungTest.java
@@ -1,6 +1,7 @@
 import net.monternos.kundenverwaltung.Knoten;
 import net.monternos.kundenverwaltung.Kundeneintrag;
 import net.monternos.kundenverwaltung.Kundenverwaltung;
+import net.monternos.kundenverwaltung.Person;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,20 +9,23 @@ import static org.junit.Assert.assertEquals;
 
 public class KundenverwaltungTest {
   private Kundenverwaltung kundenverwaltung;
+  private Person person;
 
   @Before
   public void setUp() {
+    person = new Person("John", "Smith", "Austria", (byte) 20);
     kundenverwaltung = new Kundenverwaltung();
-    kundenverwaltung.setWurzel(new Knoten(new Kundeneintrag("John", "Smith")));
+    kundenverwaltung.setWurzel(new Knoten(new Kundeneintrag((short) 200, person)));
   }
 
   @Test
-  public void assumeVornameOfWurzelIsCorrect() {
-    assertEquals(((Kundeneintrag) kundenverwaltung.getWurzel().getDaten()).getVorname(), "John");
+  public void assumeWurzelStoresCorrectPerson() {
+    assertEquals(((Kundeneintrag) kundenverwaltung.getWurzel().getDaten()).getPerson(), person);
   }
 
   @Test
-  public void assumeNachnameOfWurzelIsCorrect() {
-    assertEquals(((Kundeneintrag) kundenverwaltung.getWurzel().getDaten()).getNachname(), "Smith");
+  public void assumeWurzelStoresCorrectKundennummer() {
+    assertEquals(
+        ((Kundeneintrag) kundenverwaltung.getWurzel().getDaten()).getKundennummer(), (short) 200);
   }
 }
